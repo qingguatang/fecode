1e96ff9d7a8480e4f612b5d0d9db25ed1b23e5dd
bencode <bencode@163.com>
2018-03-29 12:07:41 +0800

    finish color mixser html and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,38 @@
 const gulp = require('gulp');
 const webserver = require('gulp-webserver');
+const less = require('gulp-less')
+const sourcemaps = require('gulp-sourcemaps');
+const LessAutoprefix = require('less-plugin-autoprefix');
+
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 
 gulp.task('webserver', function() {
-  gulp.src('app')
+  gulp.src('./app')
     .pipe(webserver({
-      livereload: true
+      livereload: true,
+      open: true,
+      directoryListing: {
+        enable:true,
+        path: 'app'
+      }
     }));
 });
 
 
-gulp.task('default', ['webserver']);
+gulp.task('less', function () {
+  gulp.src('./app/**/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./app/**/*.less', ['less']);
+});
+
+gulp.task('default', ['webserver', 'watch', 'less']);
