59e42e9a85d37f4b102725f16b3153395d6a23dd
bencode <bencode@163.com>
2018-03-30 19:05:29 +0800

    更新一下

--- app/c10/script.js
+++ app/c10/script.js
@@ -5,33 +5,33 @@ function doYourWork() {
 
 var label = document.querySelector('.color-mixer .label');
 
-// var red = 176;
+// var red = '176';
 // var green = 36;
-// var blue = 115;
+// var blue = 115;;
 
 // label.innerHTML = 'rgb(76,36,152)';
-var color ='rgb(' + red + ',' + green + ',' + blue + ')'; 
+var color = 'rgb(' + red + ',' + green + ',' + blue + ')'; 
 label.innerHTML = color;
 
-var a = document.querySelector('h2');
-a.innerHTML = '我要修改下标题';
+// var a = document.querySelector('h2');
+// a.innerHTML = '我要修改下标题';
 
-var message = 'aaabbb';
+// var message = 'aaabbb';
 
 // alert(message);
 
 var indicator = document.querySelector('.color-mixer .indicator');
 
-console.log(indicator);
+// console.log(indicator);
 
 indicator.style.backgroundColor = color; // 'rgb(' + red + ',' + green + ',' + blue + ')';
 // background-color
 
 
-label.style.fontSize = '20px';
-label.style.fontWeight = 'bold';
+// label.style.fontSize = '20px';
+// label.style.fontWeight = 'bold';
 // font-size
 
-a.style.color = 'red';
+// a.style.color = 'red';
 
 }
