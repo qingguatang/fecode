b47d8399eb716611af4c8f68457c2244736af835
bencode <bencode@163.com>
2018-04-02 13:31:01 +0800

    完成富文本编辑器

---
+++ app/c11/script.js
@@ -0,0 +1,31 @@
+var colorInput = document.querySelector('.text-editor .color-input');
+var bgColorInput = document.querySelector('.text-editor .bg-color-input');
+var fontSizeSelect = document.querySelector('.text-editor .font-size-select');
+var textInput = document.querySelector('.text-editor .text-input');
+var addBtn = document.querySelector('.text-editor .add-btn');
+var preview = document.querySelector('.text-editor .preview');
+
+console.log(colorInput);
+console.log(bgColorInput);
+console.log(fontSizeSelect);
+console.log(textInput);
+console.log(addBtn); 
+console.log(preview); 
+
+
+function addText() {
+  var color = colorInput.value;
+  var bgColor = bgColorInput.value;
+  var fontSize = fontSizeSelect.value;
+  var text = textInput.value;
+  console.log(color, bgColor, fontSize, text);
+
+  var style = 'color:' + color + ';background:' +
+      bgColor + ';font-size:' + fontSize + ';';
+  var html = '<span style='+style+'>'+text+'</span>';
+  preview.innerHTML = preview.innerHTML + html;
+  textInput.value = '';
+}
+
+
+addBtn.addEventListener('click', addText);
