f424845b4721003e5b943fa8e32cf563a37477b0
bencode <bencode@163.com>
2018-04-10 15:13:33 +0800

    update

--- app/c12/index.js
+++ app/c12/index.js
@@ -21,10 +21,28 @@ btn.addEventListener('click', function() {
   var newValue = value + 1;
 
   if (newValue > 100) {
-    alert('Success!');
+    alert('Yeah!');
     newValue = 0;
   }
 
   btn.innerHTML = newValue;
   progress.style.width = newValue + '%';
 });
+
+
+function onTimeInterval() {
+  var value = parseInt(btn.innerHTML, 10);
+  var newValue = value - 1;
+  if (newValue >= 0) {
+    btn.innerHTML = newValue;
+    progress.style.width = newValue + '%';
+
+    if (newValue == 0) {
+      setTimeout(function() {
+        alert('Oh!');
+      }, 0);
+    }
+  }
+}
+
+setInterval(onTimeInterval, 500);
