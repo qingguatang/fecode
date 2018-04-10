970beb805f8e603ae941ac84a1b2c1a3fc644f34
bencode <bencode@163.com>
2018-04-10 16:15:17 +0800

    完成时钟

--- app/c12/clock.js
+++ app/c12/clock.js
@@ -13,4 +13,28 @@ function refreshTime() {
 }
 
 
+refreshTime();
 setInterval(refreshTime, 1000);
+
+
+function setClockTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+
+  var hourPointer = document.querySelector('.clock .hour');
+  var minutePointer = document.querySelector('.clock .minute');
+  var secondPointer = document.querySelector('.clock .second');
+
+  var hourDeg = hour * 30 - 90;
+  var minuteDeg = minute * 6 - 90;
+  var secondDeg = second * 6 - 90;
+
+  hourPointer.style.transform = 'rotate('+hourDeg+'deg)';
+  minutePointer.style.transform = 'rotate('+minuteDeg+'deg)';
+  secondPointer.style.transform = 'rotate('+secondDeg+'deg)';
+}
+
+setClockTime();
+setInterval(setClockTime, 1000);
