643728cf4e268254e7b959a24c5035466494add3
bencode <bencode@163.com>
2018-04-10 15:02:41 +0800

    完成点击，示例

--- app/c12/index.js
+++ app/c12/index.js
@@ -1,9 +1,18 @@
 var btn = document.querySelector('.red-packet .btn');
+var progress = document.querySelector('.red-packet .progress .value');
+
 
 function onButtonClick() {
   var value = parseInt(btn.innerHTML, 10);
   var newValue = value + 1;
+
+  if (newValue > 100) {
+    alert('Success!');
+    newValue = 0;
+  }
+
   btn.innerHTML = newValue;
+  progress.style.width = newValue + '%';
 }
 
 btn.addEventListener('click', onButtonClick);
