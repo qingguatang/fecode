bfefb823c2411f3f8740096aa492cc3af9528aa7
bencode <bencode@163.com>
2018-04-10 15:04:50 +0800

    anonymous

--- app/c12/index.js
+++ app/c12/index.js
@@ -2,7 +2,21 @@ var btn = document.querySelector('.red-packet .btn');
 var progress = document.querySelector('.red-packet .progress .value');
 
 
-function onButtonClick() {
+// function onButtonClick() {
+//   var value = parseInt(btn.innerHTML, 10);
+//   var newValue = value + 1;
+
+//   if (newValue > 100) {
+//     alert('Success!');
+//     newValue = 0;
+//   }
+
+//   btn.innerHTML = newValue;
+//   progress.style.width = newValue + '%';
+// }
+
+
+btn.addEventListener('click', function() {
   var value = parseInt(btn.innerHTML, 10);
   var newValue = value + 1;
 
@@ -13,6 +27,4 @@ function onButtonClick() {
 
   btn.innerHTML = newValue;
   progress.style.width = newValue + '%';
-}
-
-btn.addEventListener('click', onButtonClick);
+});
