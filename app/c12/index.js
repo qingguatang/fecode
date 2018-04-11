6c4097ec2a9c2bcece31ebdc0c3698846671c364
bencode <bencode@163.com>
2018-04-11 23:41:32 +0800

    update

--- app/c12/index.js
+++ app/c12/index.js
@@ -1,48 +1,27 @@
+var progress = document.querySelector('.red-packet .progress .bar');
 var btn = document.querySelector('.red-packet .btn');
-var progress = document.querySelector('.red-packet .progress .value');
 
 
-// function onButtonClick() {
-//   var value = parseInt(btn.innerHTML, 10);
-//   var newValue = value + 1;
-
-//   if (newValue > 100) {
-//     alert('Success!');
-//     newValue = 0;
-//   }
-
-//   btn.innerHTML = newValue;
-//   progress.style.width = newValue + '%';
-// }
-
-
-btn.addEventListener('click', function() {
+function onButtonClick() {
+  console.log('hello');
   var value = parseInt(btn.innerHTML, 10);
   var newValue = value + 1;
-
-  if (newValue > 100) {
-    alert('Yeah!');
-    newValue = 0;
-  }
-
   btn.innerHTML = newValue;
+
   progress.style.width = newValue + '%';
-});
-
-
-function onTimeInterval() {
-  var value = parseInt(btn.innerHTML, 10);
-  var newValue = value - 1;
-  if (newValue >= 0) {
-    btn.innerHTML = newValue;
-    progress.style.width = newValue + '%';
-
-    if (newValue == 0) {
-      setTimeout(function() {
-        alert('Oh!');
-      }, 0);
-    }
-  }
 }
 
-setInterval(onTimeInterval, 500);
+btn.addEventListener('click', onButtonClick);
+
+
+function countDown() {
+  console.log('count down');
+  var value = parseInt(btn.innerHTML, 10);
+  var newValue = value - 1;
+  btn.innerHTML = newValue;
+
+  progress.style.width = newValue + '%';
+}
+
+setInterval(countDown, 1000);
+
