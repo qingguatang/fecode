1cd92113ee5f4c63d1f876b8b8f853d71e7ad987
bencode <bencode@163.com>
2018-04-11 15:00:47 +0800

    update

--- app/c11/script.js
+++ app/c11/script.js
@@ -15,26 +15,18 @@ function addText() {
   var textInput = document.querySelector('.text-editor .text-input');
   var text = textInput.value;
 
-
   var boldCheckbox = document.querySelector('.text-editor .bold-checkbox');
-  // var fontWeight = 'normal';
-  // if (boldCheckbox.checked) {
-  //   fontWeight = 'bold';
-  // }
+  console.log(boldCheckbox.checked);
 
-  var fontWeight = boldCheckbox.checked ? 'bold' : 'normal';
-
-  var underlineCheckbox = document.querySelector('.text-editor .underline-checkbox');
-  // var decorate = 'none';
-  // if (underlineCheckbox.checked) {
-  //   decorate = 'underline';
-  // }
-
-  var decorate = underlineCheckbox.checked ? 'underline' : 'none';
+  var fontWeight = 'normal';
+  if (boldCheckbox.checked) {
+    console.log('bold');
+    fontWeight = 'bold';
+  }
 
   var lastHtml = preview.innerHTML;
-  preview.innerHTML = lastHtml + '<span style="color:' + color + 
-      '; font-weight: ' + fontWeight + '; text-decoration: ' + decorate +
+  preview.innerHTML = lastHtml + '<span style="color:' + color +
+      '; font-weight: ' + fontWeight +
       '; background: '+bgColor+'; font-size: '+
       fontSize+'">'+text+'</span>';
 
@@ -43,14 +35,16 @@ function addText() {
 
 // 2
 var addBtn = document.querySelector('.add-btn');
-
 addBtn.addEventListener('click', addText);
 
 
+
+
+// 其他例子
 function log() {
-  console.log('a');
-  console.log('b');
-  console.log('c');
+  // console.log('a');
+  // console.log('b');
+  // console.log('c');
 }
 
 // log();
