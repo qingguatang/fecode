51b97ce34044c4be4834785d2a03f2168cba1ca6
bencode <bencode@163.com>
2018-04-13 19:48:59 +0800

    完成菜单

---
+++ app/c13/menu.js
@@ -0,0 +1,26 @@
+function initMenu() {
+  // var li = document.querySelector('.menu li');
+  // console.log(li);
+  var lis = document.querySelectorAll('.menu li');
+  console.log(lis);
+
+  // 把它当作固定结构来学
+  // 1. 在你没有熟练之前，括号里面只有一个地方可以改。
+  for (var i = 0; i < lis.length; i++) {
+    // 如果循环里面有处理事件
+    // 就把它放到旁边的函数中
+    addEvent(lis[i]);
+  }
+
+  function addEvent(li) {
+    li.addEventListener('click', function() {
+      for (var i = 0; i < lis.length; i++) {
+        lis[i].classList.remove('active');
+      }
+      li.classList.add('active');
+    });
+  }
+}
+
+
+initMenu();
