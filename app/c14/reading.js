fdb7eacdbc35c105644ba0178aeb20ff8a0b50d2
bencode <bencode@163.com>
2018-04-19 15:54:05 +0800

    完成功能

--- app/c14/reading.js
+++ app/c14/reading.js
@@ -43,10 +43,92 @@ function handleArticleEvent() {
   function addEvent(index) {
     spans[index].addEventListener('dblclick', function() {
       console.log(spans[index]);
+      addNewWord(spans[index].innerHTML);
     });
   }
 }
 
+
+function addNewWord(word) {
+  console.log(word);
+  var li = document.createElement('li');
+
+  // li.style.background = '#f00'
+  li.classList.add('list-group-item');
+  li.innerHTML = word;
+ 
+  wordsList.appendChild(li);
+
+  handleNewWordEvent(li);
+}
+
+
+function handleNewWordEvent(li) {
+  li.addEventListener('click', function() {
+    var lis = wordsList.querySelectorAll('li');
+    for (var i = 0; i < lis.length; i++) {
+      lis[i].classList.remove('active');
+    }
+    li.classList.add('active');
+  });
+}
+
+
+var lis = wordsList.querySelectorAll('li');
+for (var i = 0; i < lis.length; i++) {
+  handleNewWordEvent(lis[i]);
+}
+
+
+var deleteButton = app.querySelector('.remove');
+deleteButton.addEventListener('click', function() {
+  var li = wordsList.querySelector('.active');
+  li.parentNode.removeChild(li);
+});
+
+
+var upButton = app.querySelector('.up');
+upButton.addEventListener('click', function() {
+  var li = wordsList.querySelector('.active');
+  if (!li) {
+    return;
+  }
+  
+  var lis = wordsList.querySelectorAll('li');
+
+  var index = indexOf(lis, li);
+  var prev = lis[index - 1];
+  console.log('prev', prev);
+  if (!prev) {
+    return;
+  }
+  li.parentNode.insertBefore(li, prev);
+});
+
+var downButton = app.querySelector('.down');
+downButton.addEventListener('click', function() {
+  var li = wordsList.querySelector('.active');
+  if (!li) {
+    return;
+  }
+  
+  var lis = wordsList.querySelectorAll('li');
+
+  var index = indexOf(lis, li);
+  var next = lis[index + 2];
+  console.log('next', next);
+  li.parentNode.insertBefore(li, next);
+});
+
+
+function indexOf(lis, li) {
+  for (var i = 0; i < lis.length; i++) {
+    if (lis[i] == li) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 function isWord(work) {
   return (/^[-\w]+$/).test(work);
