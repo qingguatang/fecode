7525d7412643918f8d8c5c5d49a3f273ca35ba9a
bencode <bencode@163.com>
2018-04-25 16:19:48 +0800

    临时保存

--- app/c13/tab.js
+++ app/c13/tab.js
@@ -1,28 +1,17 @@
-function initTab() {
-  var tab = document.querySelector('.tab');
-  var lis = tab.querySelectorAll('.navs li');
-  var panes = tab.querySelectorAll('.pane');
+var tab = document.querySelector('.tab');
+var lis = tab.querySelectorAll('.navs li');
+var panes = tab.querySelectorAll('.pane');
 
-  for (var i = 0; i < lis.length; i++) {
-    addEvent(lis[i], i);
-  }
-
-  function addEvent(li, index) {
-    li.addEventListener('click', function() {
-      for (var i = 0; i < lis.length; i++) {
-        lis[i].classList.remove('active');
-        panes[i].style.display = 'none';
-      }
-      li.classList.add('active');
-      panes[index].style.display = 'block';
-    });
-  }
-
-  // 1. 使用CSS把初始状态弄好
-
-  // 主动触发一个事件
-  var event = new MouseEvent('click');
-  lis[0].dispatchEvent(event);
+for (var i = 0; i < lis.length; i++) {
+  addEvent(i);
 }
 
-initTab();
+function addEvent(index) {
+  var li = lis[index];
+  li.addEventListener('click', function() {
+    for (var i = 0; i < lis.length; i++) {
+      lis[i].classList.remove('active');
+    }
+    li.classList.add('active');
+  });
+}
---
+++ app/tmp/tab.1.js
@@ -0,0 +1,29 @@
+function initTab() {
+  var tab = document.querySelector('.tab');
+  var lis = tab.querySelectorAll('.navs li');
+  var panes = tab.querySelectorAll('.pane');
+
+  for (var i = 0; i < lis.length; i++) {
+    addEvent(lis[i], i);
+  }
+
+  function addEvent(li, index) {
+    li.addEventListener('click', function() {
+      for (var i = 0; i < lis.length; i++) {
+        lis[i].classList.remove('active');
+        panes[i].style.display = 'none';
+      }
+      li.classList.add('active');
+      panes[index].style.display = 'block';
+    });
+  }
+
+  // 1. 使用CSS把初始状态弄好
+
+  // 主动触发一个事件
+  var event = new MouseEvent('click');
+  var li = tab.querySelector('.navs li.active');
+  li.dispatchEvent(event);
+}
+
+initTab();
