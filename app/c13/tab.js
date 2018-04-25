41fcaea990059602537477fc088ef87a65adc7ea
bencode <bencode@163.com>
2018-04-25 18:17:22 +0800

    c2

--- app/c13/tab.js
+++ app/c13/tab.js
@@ -1,17 +1,28 @@
-var tab = document.querySelector('.tab');
-var lis = tab.querySelectorAll('.navs li');
-var panes = tab.querySelectorAll('.pane');
+initTab();
 
-for (var i = 0; i < lis.length; i++) {
-  addEvent(i);
-}
+function initTab() {
+  var tab = document.querySelector('.tab');
+  var lis = tab.querySelectorAll('.navs li');
+  var panes = tab.querySelectorAll('.pane');
 
-function addEvent(index) {
-  var li = lis[index];
-  li.addEventListener('click', function() {
-    for (var i = 0; i < lis.length; i++) {
-      lis[i].classList.remove('active');
-    }
-    li.classList.add('active');
-  });
+  for (var i = 0; i < lis.length; i++) {
+    addEvent(i);
+  }
+
+  function addEvent(index) {
+    var li = lis[index];
+    li.addEventListener('click', function() {
+      for (var i = 0; i < lis.length; i++) {
+        lis[i].classList.remove('active');
+        panes[i].style.display = 'none';
+      }
+      li.classList.add('active');
+      panes[index].style.display = 'block';
+    });
+  }
+
+  var event = new Event('click');
+  // lis[0].dispatchEvent(event);
+
+  tab.querySelector('.navs li.active').dispatchEvent(event);
 }
