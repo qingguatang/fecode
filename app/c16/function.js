a39378b902be8ac2afa8c870320ef35e092a219e
bencode <bencode@163.com>
2018-05-12 11:34:54 +0800

    finish func

---
+++ app/c16/function.js
@@ -0,0 +1,49 @@
+var success;
+var str = 'hello';
+var num = 123;
+var ok = true;
+
+hello();
+
+success = function(data) {
+  console.log('ok', data);
+};
+
+var opts = {
+  name: 'hello',
+  age: 12,
+  save: function() {
+    
+  }
+};
+
+success(opts);
+
+document.addEventListener('click', function() {
+  
+});
+
+var list = [1, 2, 3];
+var func = [
+  function() {
+    
+  },
+  function() {
+    
+  }
+]
+
+
+function hello() {
+  console.log('hello');
+  function world() {
+    var list = [];
+    for (var i = 0; i < list.length; i++) {
+      function ok() {
+        
+      }
+    }
+  }
+}
+
+hello();
