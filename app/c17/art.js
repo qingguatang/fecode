7c342a39c97fc5d5b118214517f993c66b47ba1a
bencode <bencode@163.com>
2018-05-16 22:55:46 +0800

    计算还没搞定哦

--- app/c17/art.js
+++ app/c17/art.js
@@ -5,22 +5,42 @@ var art = (function() {
 
   exports.on = function(el, selector, event, fn) {
     el.addEventListener(event, function(e) {
-      if (matches(el, e.target, selector)) {
-        fn.apply(this, arguments);
+      if (childMatch(el, e.target, selector)) {
+        fn.apply(e.target, arguments);
       }
     });
   };
 
+  exports.closest = function(el, selector) {
+    while (el) {
+      if (matches(el, selector)) {
+        return el;
+      }
+      el = el.parentNode;
+    }
+    return null;
+  };
+
+
+  exports.remove = function(el) {
+    el.parentNode.removeChild(el);
+  }
+
   return exports
 
 
-  function matches(parent, el, selector) {
-    while(el && el !== parent) {
-      if (el.matches(selector)) {
+  function childMatch(parent, el, selector) {
+    while (el && el !== parent) {
+      if (matches(el, selector)) {
         return true;
       }
       el = el.parentNode;
     }
     return false;
+  }
+
+
+  function matches(el, selector) {
+    return typeof el.matches === 'function' && el.matches(selector);
   }
 })();
--- app/c17/numbers.js
+++ app/c17/numbers.js
@@ -1,9 +1,42 @@
 (function() {
 
-  const app = document.querySelector('.numbers');
+  var app = document.querySelector('.numbers');
 
-  art.on(app, 'td', 'click', function(e) {
-    console.log(e);
+  art.on(app, 'input.text', 'focusout', function() {
+    var input = this;
+    var value = input.value.trim();
+    var td = art.closest(input, 'td');
+    art.remove(input);
+    td.dataset.value = value;
+    td.innerHTML = getText(value);
+    td.classList.remove('active');
   });
+
+  art.on(app, 'td', 'click', function() {
+    var td = this;
+    var input = document.createElement('input');
+    input.classList.add('text');
+    input.setAttribute('type', 'text');
+    input.value = td.dataset.value || '';
+    td.innerHTML = '';
+    td.classList.add('active');
+    td.appendChild(input);
+    input.focus();
+  });
+
+
+  var reCalc = /^=(\w+)\(([^)]+)\)/
+
+  function getText(value) {
+    var match = reCalc.exec(value);
+    return match ? doCalc(match[1], match[2]) : value;
+  }
+
+
+  function doCalc(name, expr) {
+    var parts = expr.split(/\s*,\s*/);
+    console.log(parts);
+    return '';
+  }
 
 })();
