649d678b9d896fc7856e752e570bb7f390a39611
bencode <bencode@163.com>
2018-05-21 23:32:05 +0800

    udpate

--- app/c17/popup.js
+++ app/c17/popup.js
@@ -3,14 +3,9 @@
   var handle = popup.querySelector('.handle');
   var panel = popup.querySelector('.panel');
 
+
   handle.addEventListener('click', function() {
     popup.classList.toggle('active');
   });
 
-  document.addEventListener('click', function() {
-    popup.classList.remove('active');
-  });
-  popup.addEventListener('click', function(e) {
-    e.stopPropagation();
-  })
 })();
