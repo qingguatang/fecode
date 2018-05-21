da32b2e41437e8ef6630f8e1768e7da4de5e633b
bencode <bencode@163.com>
2018-05-21 16:35:15 +0800

    add popup

---
+++ app/c17/popup.js
@@ -0,0 +1,10 @@
+(function() {
+  var popup = document.querySelector('.popup');
+  var handle = popup.querySelector('.handle');
+  var panel = popup.querySelector('.panel');
+
+  handle.addEventListener('click', function() {
+    popup.classList.toggle('active');
+  });
+
+})();
