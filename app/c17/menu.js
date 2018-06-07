fade91efb69cbf33c7f5274ff4d7fac1586d015a
bencode <bencode@163.com>
2018-06-07 23:16:40 +0800

    update

--- app/c17/menu.js
+++ app/c17/menu.js
@@ -24,19 +24,4 @@
     li.classList.add('active');
   });
 
-  function initMenu() {
-    var lis = menu.querySelectorAll('li');
-    for (var i = 0; i < lis.length; i++) {
-      addEvent(i);
-    }
-    function addEvent(index) {
-      var li = lis[index];
-      li.addEventListener('click', function() {
-        for (var i = 0; i < lis.length; i++) {
-          lis[i].classList.remove('active');
-        }
-        li.classList.add('active');
-      });
-    }
-  }
 })();
