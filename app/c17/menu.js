f4ec5c9b0f5942be72a8338516b07b2974bd1464
bencode <bencode@163.com>
2018-06-07 23:06:16 +0800

    update

--- app/c17/menu.js
+++ app/c17/menu.js
@@ -3,18 +3,26 @@
 
   // initMenu();
 
-  menu.addEventListener('click', function(event) {
-    console.log(event.target.tagName);
-    if (event.target.tagName == 'LI') {
-      var lis = menu.querySelectorAll('li');
-      var li = event.target;
-      for (var i = 0; i < lis.length; i++) {
-        lis[i].classList.remove('active');
-      }
-      li.classList.add('active');
-    }
-  });
+  // menu.addEventListener('click', function(event) {
+  //   console.log(event.target.tagName);
+  //   if (event.target.tagName == 'LI') {
+  //     var lis = menu.querySelectorAll('li');
+  //     var li = event.target;
+  //     for (var i = 0; i < lis.length; i++) {
+  //       lis[i].classList.remove('active');
+  //     }
+  //     li.classList.add('active');
+  //   }
+  // });
 
+  $(menu).on('click', 'li', function() {
+    var lis = menu.querySelectorAll('li');
+    var li = this; //event.target;
+    for (var i = 0; i < lis.length; i++) {
+      lis[i].classList.remove('active');
+    }
+    li.classList.add('active');
+  });
 
   function initMenu() {
     var lis = menu.querySelectorAll('li');
